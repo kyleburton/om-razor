b7a61b705b4fb2fa66aef35bd62e5d1aebd09e43
Kyle Burton <kyle.burton@gmail.com>
2014-09-27 08:45:18 -0400

    fixes #5 parse inline styles into a #js map

--- om-razor.js
+++ om-razor.js
@@ -1,15 +1,19 @@
+/*jslint indent: 2 */
+/*global $, window*/
 // n = OmRazor.domToOm('.breadcrumb')
 (function (root) {
+  "use strict";
   var OmRazor = root.OmRazor = {};
 
   OmRazor.domToOm = function (opts) {
-    var selector = opts.selector, elts = $(selector), tree, result = "(defn " + (opts.name||'widget') + " [data owner] \n";
-    
+    var selector = opts.selector, elts = $(selector), tree, result = "(defn " + (opts.name || 'widget') + " [data owner] \n";
+
     tree = OmRazor.domNodeToTree({
       elt:       elts,
       attrs:     OmRazor.namedNodeMapToObject(elts[0].attributes),
       text:      OmRazor.nodeText(elts),
-      children:  []});
+      children:  []
+    });
 
     result = OmRazor.treeToWidget(tree, result, '  ') + "\n)\n";
 
@@ -24,12 +28,45 @@
   OmRazor.attrsToCljsMap = function (attrs) {
     var pairs = [];
     $.each(attrs, function (key, val) {
+      if (key === "style") {
+        pairs.push(":" + key + " #js " + JSON.stringify(
+          OmRazor.inlineStyleToMap(val)
+        ));
+        return;
+      }
+
       if (key === "class") {
         key = "className";
       }
+
       pairs.push(":" + key + "  " + JSON.stringify(val));
-    }); 
+    });
     return '{' + pairs.join(", ") + '}';
+  };
+
+  /**
+   * NB: this is a 'best effort' simplistic parser.  To correctly parse in-line
+   * styles we need to use a full CSS parser. See:
+   *
+   *   http://stackoverflow.com/questions/3326494/parsing-css-in-javascript-jquery
+   *
+   * NB: This parser does not handle comments.
+   *
+   */
+  OmRazor.inlineStyleToMap = function (style) {
+    var res = {}, parts = style.split(';'), ii, pair;
+    for (ii = 0; ii < parts.length; ii += 1) {
+      if (parts[ii].length === 0) {
+        continue;
+      }
+      pair = parts[ii].split(':', 2);
+      if (!pair || pair.length !== 2) {
+        console.log('OmRazor.inlineStyleToMap: unrecognized style: %o', parts[ii]);
+        continue;
+      }
+      res[pair[0].trim()] = pair[1].trim();
+    }
+    return res;
   };
 
   OmRazor.treeToWidget = function (tree, buf, indent) {
@@ -60,8 +97,8 @@
   };
 
   OmRazor.nodeText = function (elt) {
-    var text = elt.contents().filter(function() {
-      return this.nodeType == 3;
+    var text = elt.contents().filter(function () {
+      return this.nodeType === 3;
     }).text();
     return OmRazor.trimText(text);
   };
